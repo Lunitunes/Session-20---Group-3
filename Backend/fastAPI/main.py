5b5cad3caf88f87d62ba0eadfc3dd672302aee8c
Meta.ig <44486676+metaig@users.noreply.github.com>
2025-11-07 13:59:28 +1100

    Implement CORS middleware and import pandas
    Added CORS middleware for frontend communication and imported pandas.

--- Backend/fastAPI/main.py
+++ Backend/fastAPI/main.py
@@ -1,15 +1,28 @@
-from fastapi import FastAPI, HTTPException, depends, request, status #the fastapi
-import os #files
-import pickle #pkl
+from fastapi import FastAPI, HTTPException, Depends, Request, status
+import os
+import pandas as pd
+import pickle
 from pydantic import BaseModel, Field, ValidationError
 from fastapi.responses import JSONResponse
 from fastapi import BackgroundTasks
 import time
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+app.middleware(
+    # frontend communication. Change details for connection.
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 model = None
 MODEL_FILENAME = "trained_model.pkl"
+# THIS FILE SHOULD BE IN THE SAME DIRECTORY AS BACKEND/FASTAPI/MAIN.PY 
 
 async def startup(app: FastAPI):
     global model
@@ -24,3 +37,28 @@ async def startup(app: FastAPI):
             raise FileNotFoundError(f"No such file or directory for {MODEL_PATH}")
     except Exception as e:
         print(f"Couldn't load model with error: {e}")
+
+class InputData(BaseModel):
+    proto: float
+    service: float
+    state: float
+    dur: float
+    sbytes: float
+    dbytes: float
+    spkts: float
+    dpkts: float
+    sttl: float
+    dttl: float
+    swin: float
+    dwin: float
+    ct_srv_src: float
+    ct_state_ttl: float
+    ct_dst_ltm: float
+    ct_src_dport_ltm: float
+    ct_dst_sport_ltm: float
+    ct_dst_src_ltm: float
+    is_ftp_login: float
+    ct_ftp_cmd: float
+    ct_flw_http_mthd: float
+
+
