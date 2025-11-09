a6057f2b798502b4b6b17ca932243120d9d03147
lunitunes <matt.bailey104@icloud.com>
2025-11-09 15:00:00 +1100

    Add components

--- Frontend/app/analysis/[analysis_id]/page.tsx
+++ Frontend/app/analysis/[analysis_id]/page.tsx
@@ -2,13 +2,28 @@ import { AnalysisSummary } from "@/types/analysis";
 
 
 export default function ChartsPage(){
+  
 
+  const chartData = 
   
 
 
   return(
     <div className="">
-      
+      <h1>Data Visualised</h1>
+
+
+      <div className="grid">
+        <div id="BarChart" className="">
+
+        </div>
+        <div id="PieChart" className="">
+
+        </div>
+        <div className="">
+
+        </div>
+      </div>
     </div>
   )
 }
